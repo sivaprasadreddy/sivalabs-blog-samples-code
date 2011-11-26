1bd2665e534fcb723ee5ce7006f01993618035c7
sivaprasadreddy <sivaprasadreddy.k@gmail.com>
2011-11-26 20:34:08 +0530

    SpringProfiles app initial codebase.

---
+++ springprofilles/src/main/java/com/sivalabs/SampleService.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.sivalabs;
+
+//import org.springframework.stereotype.Service;
+
+/**
+ * @author siva
+ *
+ */
+//@Service
+public class SampleService {
+	private String status;
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+}
---
+++ springprofilles/src/main/webapp/index.jsp
@@ -0,0 +1,4 @@
+<%
+
+response.sendRedirect("home.htm");
+%>
