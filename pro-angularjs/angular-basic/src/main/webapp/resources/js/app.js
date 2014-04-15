a07a22bf51964196ea580e89be2d8706125a785c
K. Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2014-04-15 07:54:22 +0530

    Basic filters, Http usage

--- pro-angularjs/angular-basic/src/main/webapp/resources/js/app.js
+++ pro-angularjs/angular-basic/src/main/webapp/resources/js/app.js
@@ -1,14 +1,47 @@
 var todoApp = angular.module("todoApp", []);
 
+todoApp.filter("checkedItems", function () {
+	return function (items, showComplete) {
+		var resultArr = [];
+		angular.forEach(items, function (item) {
+		if (item.done == false || showComplete == true) {
+			resultArr.push(item);
+		}
+		});
+		return resultArr;
+	};
+});
+
+var model = {
+		user: "Adam"		
+		};
+
+todoApp.run(function ($http) {
+	$http.get("resources/data/todo.json").success(function (data) {
+	model.items = data;
+	});
+});
+
 todoApp.controller("ToDoCtrl", function ($scope) {
-		var model = {
-				user: "Adam",
-				items: [{ action: "Buy Flowers", done: false },
-				{ action: "Get Shoes", done: false },
-				{ action: "Collect Tickets", done: true },
-				{ action: "Call Joe", done: false }]
-				};
 		
 		$scope.todo = model;
+		
+		$scope.incompleteCount = function () {
+			var count = 0;
+			angular.forEach($scope.todo.items, function (item) {
+			if (!item.done) { count++; }
+			});
+			return count;
+		};
+		
+		$scope.warningLevel = function () {
+			return $scope.incompleteCount() < 3 ? "label-success" : "label-warning";
+		};
+		
+		$scope.addNewItem = function (actionText) {
+			$scope.todo.items.push({ action: actionText, done: false });
+		};
 });
+
+
 		
